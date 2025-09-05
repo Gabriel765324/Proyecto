2cb5547e3753e9bd4ce4064530ab526e9e208e5d
Gabriel765324 <gabrielalbertocruznogales@gmail.com>
2025-09-04 21:06:30 -0400

    Finalización de Cursos.php.

--- Cursos.php
+++ Cursos.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cursos</title>
+    <link rel="stylesheet" href="Estilo/Cursos.css">
 </head>
 <body>
     <?php
         include("Plantilla.php");
         include("Conectar22.php");
     ?>
-    <table>
-        <tr>
-            <td><img src="/Proyecto/Fotos/Introducci_n.jpeg" alt="Introducción."><br>Introducción a C++.</td>
-            <td><img src="/Proyecto/Fotos/Arreglos.png" alt="Arreglos."><br>Arreglos.</td>
-            <td><img src="/Proyecto/Fotos/" alt=""> Grafos.</td>
-            <td>Otros conceptos.</td>
-        </tr>
-    </table>
+    <div class="Tabla">
+        <div class="Fila">
+            <a href="Introducci_n.php"><div class="Celda1"><br><br><br><br><br><br>Introducción a C++.</div></a>
+            <a href="Arreglos.php"><div class="Celda2"><br><br><br><br><br><br>Arreglos.</div></a>
+            <a href="Grafos.php"><div class="Celda3"><br><br><br><br><br><br>Grafos.</div></a>
+            <a href="Otros.php"><div class="Celda4"><br><br><br><br><br><br>Otros conceptos.</div></a>
+        </div>
+    </div>
 </body>
 </html>
